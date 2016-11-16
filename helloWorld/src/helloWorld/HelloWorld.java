7ac9e480aefb178d202849423779b2b673482099
HH305- <HH305-@HH306-38>
2016-11-16 18:05:58 +0900

    Local Repository Add a PushPrint Java

--- helloWorld/src/helloWorld/HelloWorld.java
+++ helloWorld/src/helloWorld/HelloWorld.java
@@ -4,13 +4,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		System.out.println("HelloWorld");
 		System.out.println("Hello Everyone");
 		System.out.println("Hello Master");
 		System.out.println("Hello hotfix");
-
-
 	}
-
 }
---
+++ helloWorld/src/helloWorld/PushPrint.java
@@ -0,0 +1,10 @@
+package helloWorld;
+
+public class PushPrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("add Java Class File Push");
+	}
+
+}
